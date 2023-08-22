0c25d83cf005b78a9bd8e1905b45690b2b033e12
anshb7 <92212789+anshb7@users.noreply.github.com>
2023-08-22 22:58:15 +0530

    k

--- lib/coach/studentreview.dart
+++ lib/coach/studentreview.dart
@@ -1,3 +1,4 @@
+import 'package:animated_button/animated_button.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
@@ -191,6 +192,168 @@ class studentProfile extends StatelessWidget {
             child: AutoSizeText(
               "Training Details",
               style: Theme.of(context).textTheme.titleSmall,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AutoSizeText(
+                  "Forehand",
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+                LinearProgressIndicator(
+                  color: Theme.of(context).colorScheme.secondary,
+                  value: 0.5,
+                  minHeight: MediaQuery.of(context).size.height * 0.03,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AutoSizeText(
+                  "Backhand",
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+                LinearProgressIndicator(
+                  color: Theme.of(context).colorScheme.secondary,
+                  value: 0.5,
+                  minHeight: MediaQuery.of(context).size.height * 0.03,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AutoSizeText(
+                  "Hand-Eye Coordination",
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+                LinearProgressIndicator(
+                  color: Theme.of(context).colorScheme.secondary,
+                  value: 0.5,
+                  minHeight: MediaQuery.of(context).size.height * 0.03,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AutoSizeText(
+                  "Flexibility",
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+                LinearProgressIndicator(
+                  color: Theme.of(context).colorScheme.secondary,
+                  value: 0.5,
+                  minHeight: MediaQuery.of(context).size.height * 0.03,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AutoSizeText(
+                  "Speed",
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+                LinearProgressIndicator(
+                  color: Theme.of(context).colorScheme.secondary,
+                  value: 0.5,
+                  minHeight: MediaQuery.of(context).size.height * 0.03,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AutoSizeText(
+                  "Focus",
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+                LinearProgressIndicator(
+                  color: Theme.of(context).colorScheme.secondary,
+                  value: 0.5,
+                  minHeight: MediaQuery.of(context).size.height * 0.03,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AutoSizeText(
+                  "Reflexes",
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+                LinearProgressIndicator(
+                  color: Theme.of(context).colorScheme.secondary,
+                  value: 0.5,
+                  minHeight: MediaQuery.of(context).size.height * 0.03,
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: AnimatedButton(
+                color: Color.fromRGBO(29, 144, 255, 1),
+                height: MediaQuery.of(context).size.height * 0.05,
+                width: MediaQuery.of(context).size.width * 0.5,
+                onPressed: () {
+                  // final isValid =
+                  //     formkey.currentState?.validate();
+                  // if (isValid!) {
+                  //   formkey.currentState?.save();
+
+                  //   try {
+                  //     await _signupWithEmailAndPassword(
+                  //         context);
+                  //     // createuser();
+                  //     final snackbar = SnackBar(
+                  //       content: Text(
+                  //         "Successfully Added!",
+                  //         style: TextStyle(
+                  //             fontSize: 15,
+                  //             color: Colors.white),
+                  //       ),
+                  //     );
+                  //     ScaffoldMessenger.of(context)
+                  //         .showSnackBar(snackbar);
+                  //     // Navigator.push(
+                  //     //     context,
+                  //     //     MaterialPageRoute(
+                  //     //         builder: ((context) =>
+                  //     //             coachDashboard())));
+                  //   } catch (e) {
+                  //     print(e);
+                  //   }
+                  // }
+                },
+                child: AutoSizeText("Publish!",
+                    style: Theme.of(context).textTheme.labelSmall),
+              ),
             ),
           ),
         ])
